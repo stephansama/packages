1ea66fefa9631a67e293d55d56097670860e4f62
stephansama <stephanrandle@hotmail.com>
2025-06-12 06:39:49 -0400

    chore: added debugging log

--- scripts/ci/src/publish.js
+++ scripts/ci/src/publish.js
@@ -7,6 +7,8 @@ const scope = "@stephansama";
 
 const packages = fs.readdirSync("../../packages/");
 
+console.log(process.env.NODE_AUTH_TOKEN);
+
 for (const pkg of packages) {
 	const packageName = [scope, pkg].join("/");
 	await sh`pnpm publish --filter ${packageName}`;

23a0e74746d1faff2498a9b50d9db0387f0eab92
stephansama <stephanrandle@hotmail.com>
2025-06-12 06:14:18 -0400

    chore: updated workflow implementation / added ci scripts

---
+++ scripts/ci/src/publish.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import fs from "node:fs";
+import { $ as sh } from "zx";
+
+const scope = "@stephansama";
+
+const packages = fs.readdirSync("../../packages/");
+
+for (const pkg of packages) {
+	const packageName = [scope, pkg].join("/");
+	await sh`pnpm publish --filter ${packageName}`;
+}

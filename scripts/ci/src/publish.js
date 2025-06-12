689b86b6427ef01befe4391faa561b99b8050562
stephansama <stephanrandle@hotmail.com>
2025-06-12 07:29:25 -0400

    chore: updated workflow implementation

--- scripts/ci/src/publish.js
+++ scripts/ci/src/publish.js
@@ -7,8 +7,6 @@ const scope = "@stephansama";
 
 const packages = fs.readdirSync("../../packages/");
 
-console.log(process.env.NODE_AUTH_TOKEN);
-
 for (const pkg of packages) {
 	const packageName = [scope, pkg].join("/");
 	await sh`pnpm publish --filter ${packageName}`;

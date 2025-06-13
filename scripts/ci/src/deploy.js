d44badf542f9dc2975dd4876b5d57724881f1bbc
stephansama <stephanrandle@hotmail.com>
2025-06-12 23:13:38 -0400

    chore: updated ci implementation

--- scripts/ci/src/deploy.js
+++ scripts/ci/src/deploy.js
@@ -2,12 +2,10 @@ import { $ as sh } from "zx";
 
 import root from "../../../package.json" with { type: "json" };
 
-await sh`../../node_modules/.bin/typedoc`;
-
 const branch = process.env.GITHUB_HEAD_REF || "main";
 const { site } = root.config;
 
 /** @see https://developers.cloudflare.com/workers/wrangler/commands/#deploy-1 */
 await sh`
-node_modules/.bin/wrangler pages deploy ../../../dist --branch=${branch} --project-name=${site} --commit-dirty=true
+node_modules/.bin/wrangler pages deploy ../../dist --branch=${branch} --project-name=${site} --commit-dirty=true
 `;

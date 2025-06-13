f1914bbc81f24253c83761703f8316b571ce1314
stephansama <stephanrandle@hotmail.com>
2025-06-12 21:57:37 -0400

    chore: updated publish

--- scripts/ci/src/publish.js
+++ scripts/ci/src/publish.js
@@ -3,25 +3,11 @@
 import fs from "node:fs";
 import { $ as sh } from "zx";
 
-const examples = fs.readdirSync("../../examples/");
 const packages = fs.readdirSync("../../packages/");
-
-const noExamples = packages.filter((pkg) => !examples.includes(pkg));
 
 for (const pkg of packages) {
 	const packageName = ["@stephansama", pkg].join("/");
 	await sh`pnpm publish --filter ${packageName}`;
 }
 
-const templates = examples
-	.map(
-		(example) =>
-			`'../../packages/${example}' --template '../../examples/${example}/*'`,
-	)
-	.join(" ");
-
-const noTemplates = noExamples
-	.map((pkg) => `'../../packages/${pkg}'`)
-	.join(" ");
-
-await sh`../../node_modules/.bin/pkg-pr-new publish --packageManager="pnpm" --pnpm ${templates} ${noTemplates}`;
+await sh`../../node_modules/.bin/pkg-pr-new publish --packageManager="pnpm" --pnpm '../../packages/*' --template '../../examples/*'`;

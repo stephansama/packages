261196d1a0040f7b95cc6bc460a396ec27941de0
stephansama <stephanrandle@hotmail.com>
2025-06-12 22:59:28 -0400

    chore: added typedoc deploy to ci

---
+++ scripts/ci/src/deploy.js
@@ -0,0 +1,13 @@
+import { $ as sh } from "zx";
+
+import root from "../../../package.json";
+
+await sh`../../node_modules/.bin/typedoc`;
+
+const branch = process.env.GITHUB_HEAD_REF || "main";
+const { site } = root.config;
+
+/** @see https://developers.cloudflare.com/workers/wrangler/commands/#deploy-1 */
+await sh`
+node_modules/.bin/wrangler pages deploy ../../dist --branch=${branch} --project-name=${site} --commit-dirty=true
+`;
--- scripts/ci/src/publish.js
+++ scripts/ci/src/publish.js
@@ -3,10 +3,14 @@
 import fs from "node:fs";
 import { $ as sh } from "zx";
 
+import root from "../../../package.json";
+
+const { scope } = root.config;
+
 const packages = fs.readdirSync("../../packages/");
 
 for (const pkg of packages) {
-	const packageName = ["@stephansama", pkg].join("/");
+	const packageName = [scope, pkg].join("/");
 	await sh`pnpm publish --filter ${packageName}`;
 }
 

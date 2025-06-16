0bb0cbc5583f8199d721266075c863bb77ebf39c
stephansama <stephanrandle@hotmail.com>
2025-06-15 21:20:22 -0400

    chore: updated generate script

--- packages/catppuccin-rss/scripts/generate.js
+++ packages/catppuccin-rss/scripts/generate.js
@@ -1,44 +1,46 @@
 import { flavors } from "@catppuccin/palette";
-import Handlebars from "handlebars";
+import { compile } from "handlebars";
 import fs from "node:fs";
 
 import pkg from "../package.json" with { type: "json" };
 
-const { outputFolder } = pkg.config;
+const year = new Date().getFullYear();
+const { config, name: package_name, version } = pkg;
+const { outputFolder } = config;
 
 const opts = { encoding: "utf-8" };
 
-const xml = fs.readFileSync("./template/template.xml.hbs", opts);
-const comment = fs.readFileSync("./template/comment.hbs", opts);
-const styleCss = fs.readFileSync("./template/style.css.hbs", opts);
+const hbsXml = fs.readFileSync("./template/template.xml.hbs", opts);
+const hbsComment = fs.readFileSync("./template/comment.hbs", opts);
+const hbsStyleCss = fs.readFileSync("./template/style.css.hbs", opts);
 
-const templateXml = Handlebars.compile(xml);
-const templateComment = Handlebars.compile(comment);
-const templateStyleCss = Handlebars.compile(styleCss);
+const templateXml = compile(hbsXml);
+const templateComment = compile(hbsComment);
+const templateStyleCss = compile(hbsStyleCss);
 
 if (!fs.existsSync(outputFolder)) fs.mkdirSync(outputFolder);
 
-Object.entries(flavors).forEach(([theme, val]) => {
+for (const [theme, val] of Object.entries(flavors)) {
 	const filename = outputFolder + `/${theme}.xsl`;
-	const outputStyleCss = templateStyleCss(convertColors(val.colors));
-	const outputComment = templateComment({
-		package_name: pkg.name,
-		theme,
-		version: pkg.version,
-		year: new Date().getFullYear(),
-	});
-	const outputXml = templateXml({
-		comment: outputComment,
-		style: outputStyleCss,
-	});
-	fs.writeFileSync(filename, outputXml);
-});
+	const comment = getComment(theme);
+	const style = templateStyleCss(convertColors(val.colors));
+	const xml = templateXml({ comment, style });
+	fs.writeFileSync(filename, xml);
+}
 
-/**
- * @param {typeof flavors.mocha.colors} colors
- */
+/** @param {typeof flavors.mocha.colors} colors */
 function convertColors(colors) {
 	return Object.entries(colors)
 		.map(([key, val]) => [key, val.hex])
 		.reduce((prev, [key, hex]) => ({ ...prev, [key]: hex }));
+}
+
+/** @param {keyof typeof flavors} theme */
+function getComment(theme) {
+	return templateComment({
+		package_name,
+		theme,
+		version,
+		year,
+	});
 }

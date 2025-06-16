564c258c72f5c5c0de2abdf1c076b8bd28566937
stephansama <stephanrandle@hotmail.com>
2025-06-16 00:45:43 -0400

    chore: updated example

--- packages/example/src/index.ts
+++ packages/example/src/index.ts
@@ -8,24 +8,25 @@ import path from "node:path";
 import examples from "../scripts/examples.json" with { type: "json" };
 
 export async function main() {
-	const example = await select({
-		choices: examples.map((example) => ({
-			description: example.description,
-			name: `${example.name} v${example.version}`,
-			value: example.name,
-		})),
-		message: "Select an example:",
-	});
+	const example = (
+		await select({
+			choices: examples.map((example) => ({
+				description: example.description,
+				name: `${example.name} v${example.version}`,
+				value: example.name,
+			})),
+			message: "Select an example:",
+		})
+	).replace("@example", "examples");
 
 	const dir = await input({
 		default: ".",
 		message: "Input the directory to clone to:",
 	});
 
-	await downloadTemplate(
-		`github:stephansama/packages/${example.replace("@example", "examples")}`,
-		{ cwd: path.resolve(), dir, force: true },
-	)
+	const template = `github:stephansama/packages/${example}`;
+
+	await downloadTemplate(template, { cwd: path.resolve(), dir, force: true })
 		.then(() => console.log(`successfully downloaded template to ${dir}`))
 		.catch((e) => console.error(e));
 

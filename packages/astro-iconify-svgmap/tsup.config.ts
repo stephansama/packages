de45d8dab405de3fc5ae2d0e73df6a3ce2c127b3
stephansama <stephanrandle@hotmail.com>
2025-07-09 22:40:09 -0400

    chore: moved bundle analysis to tsup

--- packages/astro-iconify-svgmap/tsup.config.ts
+++ packages/astro-iconify-svgmap/tsup.config.ts
@@ -5,6 +5,16 @@ export default defineConfig({
 	entry: ["src/cli.ts", "src/index.ts", "src/get.ts"],
 	external: ["virtual:iconify-svgmap"],
 	format: ["esm", "cjs"],
+	plugins: [
+		// @ts-expect-error should work with tsup
+		codecovRollupPlugin({
+			bundleName: "astro-iconify-svg-bundle",
+			enableBundleAnalysis: true,
+			gitService: "github",
+			telemetry: false,
+			uploadToken: process.env.CODECOV_TOKEN,
+		}),
+	],
 	sourcemap: true,
 	splitting: false,
 	target: "esnext",
--- packages/example/tsup.config.ts
+++ packages/example/tsup.config.ts
@@ -1,9 +1,20 @@
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 import { defineConfig } from "tsup";
 
 export default defineConfig({
 	dts: true,
 	entry: ["src/*.ts"],
 	format: ["esm", "cjs"],
+	plugins: [
+		// @ts-expect-error should work with tsup
+		codecovVitePlugin({
+			bundleName: "packages-bundle",
+			enableBundleAnalysis: true,
+			gitService: "github",
+			telemetry: false,
+			uploadToken: process.env.CODECOV_TOKEN,
+		}),
+	],
 	sourcemap: true,
 	splitting: false,
 	target: "esnext",

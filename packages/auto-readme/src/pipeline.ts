04c5b3f34ac6b6f68ab00621299e9401113330c7
stephansama <stephanrandle@hotmail.com>
2025-08-10 01:36:53 -0400

    chore: added remark toc

--- packages/auto-readme/src/pipeline.ts
+++ packages/auto-readme/src/pipeline.ts
@@ -1,13 +1,20 @@
 import { remark } from "remark";
+import remarkToc from "remark-toc";
 
 import type { ActionData } from "./data";
 import type { Config } from "./schema";
 
+import { INFO } from "./log";
 import { autoReadmeRemarkPlugin } from "./plugin";
 
 export async function parse(file: string, config: Config, data: ActionData) {
-	const vfile = await remark()
-		.use(autoReadmeRemarkPlugin, config, data)
-		.process(file);
+	const pipeline = remark().use(autoReadmeRemarkPlugin, config, data);
+
+	if (config.useToc) {
+		INFO("generating table of contents");
+		pipeline.use(remarkToc, { heading: "contents" });
+	}
+
+	const vfile = await pipeline.process(file);
 	return vfile.toString();
 }

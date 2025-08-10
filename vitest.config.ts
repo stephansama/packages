214da3b0644fd21163edc5dd5ac6fee3911de0ca
stephansama <stephanrandle@hotmail.com>
2025-08-10 16:43:27 -0400

    chore: readded codecov vite plugin

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,16 @@
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+	plugins: [
+		codecovVitePlugin({
+			bundleName: "packages-bundle",
+			enableBundleAnalysis: true,
+			gitService: "github",
+			telemetry: false,
+			uploadToken: process.env.CODECOV_TOKEN,
+		}),
+	],
 	test: {
 		coverage: {
 			include: ["core/*/src/**/*.{js,jsx,ts,tsx}"],

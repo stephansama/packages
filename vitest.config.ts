a0ce9c7843c9dcb70ff25b23a7ca19eba8561b79
Stephan Randle <stephanrandle@hotmail.com>
2025-08-10 23:48:33 -0400

    chore: updated repo implementation (#67)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,21 +1,13 @@
-import { codecovVitePlugin } from "@codecov/vite-plugin";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-	plugins: [
-		codecovVitePlugin({
-			bundleName: "packages-bundle",
-			enableBundleAnalysis: true,
-			gitService: "github",
-			telemetry: false,
-			uploadToken: process.env.CODECOV_TOKEN,
-		}),
-	],
 	test: {
 		coverage: {
 			include: ["core/*/src/**/*.{js,jsx,ts,tsx}"],
 			provider: "v8",
 			reporter: ["html", "json", "text"],
 		},
+		outputFile: { junit: "./coverage/test-report.junit.xml" },
+		reporters: ["default", "junit"],
 	},
 });

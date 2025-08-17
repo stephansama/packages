cc6a21a6272145d79c6aea571cba54ace82fdd61
stephansama <stephanrandle@hotmail.com>
2025-08-17 09:56:23 -0400

    chore: switched example cli to use clack

--- core/example/src/index.ts
+++ core/example/src/index.ts
@@ -1,33 +1,60 @@
 import { downloadTemplate } from "@bluwy/giget-core";
-import { input, select } from "@inquirer/prompts";
+import {
+	cancel,
+	intro,
+	isCancel,
+	outro,
+	select,
+	spinner,
+	text,
+} from "@clack/prompts";
 import * as path from "node:path";
 
 import examples from "../dist/examples.json" with { type: "json" };
 
 export async function main() {
-	const example = (
-		await select({
-			choices: examples.map((example) => ({
-				description: example.description,
-				name: `${example.name} v${example.version}`,
-				value: example.name,
-			})),
-			message: "Select an example:",
-		})
-	).replace("@example", "examples");
+	intro("create @stephansama example projects");
 
-	const dir = await input({
-		default: ".",
-		message: "Input the directory to clone to:",
+	const example = await select({
+		message: "Select an example:",
+		options: examples.map((example) => {
+			const name = example.name.replace("@example/", "");
+			return {
+				description: example.description,
+				label: `${name} v${example.version}`,
+				value: name,
+			};
+		}),
 	});
 
-	const template = `github:stephansama/packages/${example}`;
+	if (isCancel(example)) {
+		cancel("Operation cancelled");
+		return process.exit(0);
+	}
 
-	await downloadTemplate(template, { cwd: path.resolve(), dir, force: true })
-		.then(() => console.info(`successfully downloaded template to ${dir}`))
-		.catch((e) => console.error(e));
+	const defaultDir = `./${example}`;
 
-	console.info(
-		"\n\nplease run (npm|yarn|pnpm|bun) install command to finish installation",
-	);
+	const dir = await text({
+		defaultValue: defaultDir,
+		message: "Input the directory to clone to:",
+		placeholder: defaultDir,
+	});
+
+	if (isCancel(dir)) {
+		cancel("Operation cancelled");
+		return process.exit(0);
+	}
+
+	const s = spinner();
+	s.start("Downloading template");
+
+	await downloadTemplate(`github:stephansama/packages/examples/${example}`, {
+		cwd: path.resolve(),
+		dir,
+		force: true,
+	}).catch((e) => console.error(e));
+
+	s.stop("Downloaded example");
+
+	outro(`successfully downloaded example template to ${dir}`);
 }

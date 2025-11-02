136a2361ce09b8c3471a99b5e702bafeedea47d8
stephansama <stephanrandle@hotmail.com>
2025-11-01 22:50:43 -0400

    chore: destructured argument

--- core/svelte-social-share-links/src/lib/networks.ts
+++ core/svelte-social-share-links/src/lib/networks.ts
@@ -27,24 +27,11 @@ export type UrlProps = {
 };
 
 /** @see {https://github.com/stefanobartoletti/nuxt-social-share/blob/311b65871627736f0db8120ecc7e32def78a3b3d/src/runtime/useSocialShare.ts#L45-L64} */
-export function buildUrl(selectedNetwork: NetworkSchema, props: UrlProps) {
-	const { shareUrl } = selectedNetwork;
-	const argTitle =
-		selectedNetwork.args?.title && props.title
-			? selectedNetwork.args?.title
-			: "";
-	const argUser =
-		selectedNetwork.args?.user && props.user
-			? selectedNetwork.args?.user
-			: "";
-	const argHashtags =
-		selectedNetwork.args?.hashtags && props.hashtags
-			? selectedNetwork.args?.hashtags
-			: "";
-	const argImage =
-		selectedNetwork.args?.image && props.image
-			? selectedNetwork.args?.image
-			: "";
+export function buildUrl({ args, shareUrl }: NetworkSchema, props: UrlProps) {
+	const argTitle = args?.title && props.title ? args?.title : "";
+	const argUser = args?.user && props.user ? args?.user : "";
+	const argHashtags = args?.hashtags && props.hashtags ? args?.hashtags : "";
+	const argImage = args?.image && props.image ? args?.image : "";
 
 	// Replace placeholders with actual values (encode all parameters for URL safety)
 	const template = shareUrl + argTitle + argUser + argHashtags + argImage;

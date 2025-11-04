18baf1cad92519ca3f30d39306cdc4ba4ddb6e83
stephansama <stephanrandle@hotmail.com>
2025-11-04 13:35:58 -0500

    chore: initial commit for various bugfixes

--- examples/svelte-social-share-links/svelte-kit/svelte.config.js
+++ examples/svelte-social-share-links/svelte-kit/svelte.config.js
@@ -1,10 +1,10 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({ assets: "dist", pages: "dist" }),
 	},
 
 	preprocess: vitePreprocess(),

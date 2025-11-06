7f19aabad76a428386c8b3be4c6b38e3e4f123d9
stephansama <stephanrandle@hotmail.com>
2025-11-05 19:13:33 -0500

    chore: updated unit tests

--- core/svelte-social-share-links/src/lib/networks.test.ts
+++ core/svelte-social-share-links/src/lib/networks.test.ts
@@ -19,5 +19,24 @@ it.each<[module.Network, module.UrlProps, string]>([
 		"https://www.reddit.com/submit?url=https%3A%2F%2Fstephansama.info",
 	],
 ])("should build the correct share URL for %s", (key, props, expected) => {
-	expect(module.buildUrl(networks[key], props)).toBe(expected);
+	expect(module.buildUrl(key, props)).toBe(expected);
+});
+
+it.each<[module.Network, module.UrlProps, string]>([
+	[
+		"bluesky",
+		{
+			url: "https://stephansama.info",
+		},
+		"https://bsky.app/intent/compose?text=https%3A%2F%2Fstephansama.info",
+	],
+	[
+		"reddit",
+		{
+			url: "https://stephansama.info",
+		},
+		"https://www.reddit.com/submit?url=https%3A%2F%2Fstephansama.info",
+	],
+])("should build the correct share URL for %s", (key, props, expected) => {
+	expect(module.buildUrlFromSchema(networks[key], props)).toBe(expected);
 });

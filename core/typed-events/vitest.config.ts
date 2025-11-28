d8f566b27d74b1dea337ed9c4ef136e898660d11
stephansama <stephanrandle@hotmail.com>
2025-11-27 20:45:35 -0500

    chore: updated tsconfig paths to vitest

--- core/typed-events/vitest.config.ts
+++ core/typed-events/vitest.config.ts
@@ -1,6 +1,8 @@
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineProject } from "vitest/config";
 
 export default defineProject({
+	plugins: [tsconfigPaths()],
 	test: {
 		environment: "jsdom",
 	},

485971ba0bd893b87acaece53e4bd89e29e6494c
stephansama <stephanrandle@hotmail.com>
2025-12-20 15:02:45 -0500

    chore: removed unnecessary map in util function

--- core/catppuccin-xsl/src/utils.ts
+++ core/catppuccin-xsl/src/utils.ts
@@ -5,15 +5,13 @@ import * as path from "node:path";
 import * as url from "node:url";
 
 export function convertColors(colors: CatppuccinColors) {
-	return Object.entries(colors)
-		.map(([key, val]) => [key, val.hex])
-		.reduce(
-			(acc, [key, hex]) => {
-				acc[key as keyof typeof acc] = hex;
-				return acc;
-			},
-			{} as Record<ColorName, string>,
-		);
+	return Object.entries(colors).reduce(
+		(acc, [key, val]) => {
+			acc[key as keyof typeof acc] = val.hex;
+			return acc;
+		},
+		{} as Record<ColorName, string>,
+	);
 }
 
 export async function writeFile(filename: string, contents: string) {

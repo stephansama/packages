5f0cf679780f4dcaf766bb55b2db63dd21f7354f
stephansama <stephanrandle@hotmail.com>
2026-02-16 02:11:49 -0500

    perf(core/typed-events): fix noexternal duplication in tsdown config

--- core/typed-events/tsdown.config.ts
+++ core/typed-events/tsdown.config.ts
@@ -3,10 +3,10 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
 	attw: { profile: "node16" },
 	dts: true,
-	noExternal: ["nanoid"],
 	entry: ["src/index.ts", "src/errors.ts", "src/react.ts"],
 	exports: true,
 	format: ["esm", "cjs"],
+	noExternal: ["nanoid"],
 	publint: true,
 	target: "esnext",
 });

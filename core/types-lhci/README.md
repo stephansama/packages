34c80bacaf16626b9364d89ba85e26e5884d72d4
stephansama <stephanrandle@hotmail.com>
2026-02-15 22:39:23 -0500

    chore: add types-lhci and related files
    ```
    chore: add types-lhci and related files
    ```

---
+++ core/types-lhci/README.md
@@ -0,0 +1,48 @@
+# @stephansama/types-lhci
+
+[![Source code](https://img.shields.io/badge/Source-666666?style=flat&logo=github&label=Github&labelColor=211F1F)](https://github.com/stephansama/packages/tree/main/core/types-lhci)
+[![Documentation](https://img.shields.io/badge/Documentation-211F1F?style=flat&logo=Wikibooks&labelColor=211F1F)](https://packages.stephansama.info/api/@stephansama/types-lhci)
+[![NPM Version](https://img.shields.io/npm/v/%40stephansama%2Ftypes-lhci?logo=npm&logoColor=red&color=211F1F&labelColor=211F1F)](https://www.npmjs.com/package/@stephansama/types-lhci)
+[![npm downloads](https://img.shields.io/npm/dw/@stephansama/types-lhci?labelColor=211F1F)](https://www.npmjs.com/package/@stephansama/types-lhci)
+
+types for lhci configuration
+
+##### Table of contents
+
+<details><summary>Open Table of contents</summary>
+
+- [Installation](#installation)
+- [Usage](#usage)
+
+</details>
+
+## Installation
+
+```sh
+pnpm install @stephansama/types-lhci
+```
+
+## Usage
+
+```javascript
+import { lhciSchema } from "@stephansama/types-lhci";
+
+/** @type {import('../dist/index.d.cts').LhciSchema} */
+const config = {
+  ci: {
+    upload: {
+      githubAppToken: process.env.GITHUB_APP_TOKEN || "fallback",
+      githubToken: process.env.GITHUB_TOKEN || "fallback",
+      serverBaseUrl: "https://lhci.example.com",
+      target: "lhci",
+      token: "project-token",
+    },
+  },
+};
+```
+
+you can also verify the config object later
+
+```javascript
+lhciSchema.parse(config);
+```
---
+++ core/types-lhci/src/index.ts
@@ -0,0 +1,127 @@
+import * as z from "zod";
+
+export type LhciBasicAuthSchema = z.infer<typeof lhciBasicAuthSchema>;
+export const lhciBasicAuthSchema = z.object({
+	password: z.string(),
+	username: z.string(),
+});
+
+export type LhciAssertSchema = z.infer<typeof lhciAssertSchema>;
+export const lhciAssertSchema = z.object({
+	assertions: z.record(
+		z.string(),
+		z
+			.tuple([
+				z.enum(["warn", "error"]),
+				z.object({
+					maxLength: z.number(),
+					minScore: z.number(),
+				}),
+			])
+			.or(z.enum(["off"])),
+	),
+	budgetsFile: z.string(),
+	includePassedAssertions: z.boolean(),
+	preset: z.enum([
+		"lighthouse:all",
+		"lighthouse:recommended",
+		"lighthouse:no-pwa",
+	]),
+});
+
+export type LhciCollectSchema = z.infer<typeof lhciCollectSchema>;
+export const lhciCollectSchema = z.object({
+	additive: z.boolean(),
+	chromePath: z.string(),
+	headful: z.boolean(),
+	isSinglePageApplication: z.boolean(),
+	method: z.enum(["node", "psi"]).or(z.string()).default("node"),
+	numberOfRuns: z.number(),
+	/** @see https://github.com/puppeteer/puppeteer/blob/aa246973b96c36768bf3d4db0383f7101a1b4ee9/docs/api.md#puppeteerlaunchoptions */
+	puppeteerLaunchOptions: z.object({
+		devtools: z.boolean(),
+		handleSIGHUP: z.boolean().default(true),
+		handleSIGINT: z.boolean().default(true),
+		handleSIGTERM: z.boolean().default(true),
+		pipe: z.boolean().default(false),
+		timeout: z.number().default(30000),
+	}),
+	puppeteerScript: z.string(),
+	settings: z.object({}),
+	startServerCommand: z.string(),
+	staticDirFileDiscoveryDepth: z.number(),
+	staticDistDir: z.string().optional(),
+	url: z.array(z.string()),
+});
+
+export type LhciUploadSchema = z.infer<typeof lhciUploadSchema>;
+export const lhciUploadSchema = z
+	.discriminatedUnion("target", [
+		z.object({
+			basicAuth: lhciBasicAuthSchema.optional(),
+			extraHeaders: z.record(z.string(), z.string()).optional(),
+			ignoreDuplicateBuildFailure: z.boolean().optional(),
+			serverBaseUrl: z.string().default("http://localhost:9001/"),
+			target: z.literal("lhci"),
+			token: z.string(),
+			urlReplacementPatterns: z
+				.array(z.string())
+				.default([
+					"s#:[0-9]{3,5}/#:PORT/#",
+					"s/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/UUID/ig",
+				]),
+		}),
+		z.object({
+			outputDir: z.string(),
+			reportFilenamePattern: z
+				.string()
+				.default(
+					"%%HOSTNAME%%-%%PATHNAME%%-%%DATETIME%%.report.%%EXTENSION%%",
+				),
+			target: z.literal("filesystem"),
+		}),
+		z.object({
+			target: z.literal("temporary-public-storage"),
+			uploadUrlMap: z.boolean().default(true),
+		}),
+	])
+	.and(
+		z.object({
+			githubApiHost: z.string().default("https://api.github.com"),
+			githubAppToken: z.string(),
+			githubStatusContextSuffix: z.string().optional(),
+			githubToken: z.string(),
+		}),
+	);
+
+export type LhciServerSchema = z.infer<typeof lhciServerSchema>;
+export const lhciServerSchema = z.object({
+	basicAuth: lhciBasicAuthSchema,
+	logLevel: z.enum(["silent", "verbose"]).default("verbose"),
+	port: z.number(),
+	storage: z.object({
+		sqlConnectionSsl: z.boolean().default(false),
+		sqlConnectionUrl: z.string(),
+		sqlDangerouslyResetDatabase: z.boolean().default(false),
+		sqlDatabasePath: z.string(),
+		sqlDialect: z.enum(["sqlite", "postgres", "mysql"]).default("sqlite"),
+		sqlMigrationOptions: z.object({ tableName: z.string() }),
+	}),
+});
+
+export type LhciWizardSchema = z.infer<typeof lhciWizardSchema>;
+export const lhciWizardSchema = z.object({
+	extraHeaders: z.string().optional(),
+	serverBaseUrl: z.string().optional(),
+});
+
+export type LhciSchema = z.infer<typeof lhciSchema>;
+export const lhciSchema = z.object({
+	ci: z.object({
+		assert: lhciAssertSchema.optional(),
+		collect: lhciCollectSchema.optional(),
+		server: lhciServerSchema.optional(),
+		upload: lhciUploadSchema.optional(),
+		wizard: lhciWizardSchema.optional(),
+	}),
+});

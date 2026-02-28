f5e03774d3c96ad7d4b679d43e29b2360307d810
stephansama <stephanrandle@hotmail.com>
2026-02-28 18:28:01 -0500

    chore(cli): add error handling to cli entrypoint

--- turbo/generators/templates/cli/cli.mjs
+++ turbo/generators/templates/cli/cli.mjs
@@ -2,4 +2,9 @@
 
 "use strict";
 
-import("./dist/index.js").then((mod) => mod.run());
+import("./dist/index.js")
+	.then((mod) => mod.run())
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});

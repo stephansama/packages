29b1de9a5f1f26e3573c3db46509eb20fec3f3f0
Stephan Randle <stephanrandle@hotmail.com>
2026-03-08 23:17:08 -0400

    ci(ci.js): fix exit code logic (#239)

--- core/alfred-kaomoji/ci.js
+++ core/alfred-kaomoji/ci.js
@@ -4,6 +4,6 @@ const CI = process.env.CI;
 const isBuild = process.env.npm_lifecycle_event === "build";
 
 const shouldRun = !CI && !isBuild;
-const exitCode = +shouldRun;
+const exitCode = shouldRun ? 0 : 1;
 
 process.exit(exitCode);

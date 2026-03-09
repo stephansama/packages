ecf0adc460ea37444fa65ab8b7390f6630b9616f
Stephan Randle <stephanrandle@hotmail.com>
2026-03-08 23:46:38 -0400

    feat(docs): add llm buttons and markdown config (#242)
    * feat(docs): add llm buttons and markdown config
    
    * feat(theme): import extend.css after mocha css

--- .config/www/.vitepress/theme/index.ts
+++ .config/www/.vitepress/theme/index.ts
@@ -1,3 +1,6 @@
+import type { Theme } from "vitepress";
+
+import CopyOrDownloadAsMarkdownButtons from "vitepress-plugin-llms/vitepress-components/CopyOrDownloadAsMarkdownButtons.vue";
 import DefaultTheme from "vitepress/theme";
 
 // eslint-disable-next-line
@@ -5,4 +8,14 @@ import "@catppuccin/vitepress/theme/mocha/red.css";
 
 import "./extend.css";
 
-export default DefaultTheme;
+export default {
+	enhanceApp({ app }) {
+		app.component(
+			"CopyOrDownloadAsMarkdownButtons",
+			CopyOrDownloadAsMarkdownButtons,
+		);
+	},
+	extends: DefaultTheme,
+} satisfies Theme;
+
+// export default DefaultTheme;

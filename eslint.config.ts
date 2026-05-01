87d97f6c2a72592f2555b5157c1efd37a8a20dba
stephansama <stephanrandle@hotmail.com>
2026-04-30 22:09:20 -0400

    chore(core): update package.json and dependencies

--- eslint.config.ts
+++ eslint.config.ts
@@ -17,6 +17,8 @@ export default await config(
 			{
 				files: [`examples/${globs.PKG_JSON}`],
 				rules: {
+					"node-dependencies/require-provenance-deps": "off",
+					"package-json/require-description": "off",
 					"pnpm/json-enforce-catalog": "off",
 				},
 			},

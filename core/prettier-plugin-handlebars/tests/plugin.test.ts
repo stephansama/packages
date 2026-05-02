bd7925d9912d672e9ee94739dae64f825ef3d8d4
stephansama <stephanrandle@hotmail.com>
2026-05-02 08:56:46 -0400

    build(plugin): update plugin path to index.mjs

--- core/prettier-plugin-handlebars/tests/plugin.test.ts
+++ core/prettier-plugin-handlebars/tests/plugin.test.ts
@@ -40,7 +40,7 @@ it.each(cases)(
 	async ({ filepath, input, output }) => {
 		const formatted = await prettier.format(input, {
 			filepath,
-			plugins: [new URL("../dist/index.js", import.meta.url).href],
+			plugins: [new URL("../dist/index.mjs", import.meta.url).href],
 		});
 
 		expect(formatted).not.toEqual(input);

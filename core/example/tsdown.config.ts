021873fa90d47f603a00989853c05c1136b1bf81
stephansama <stephanrandle@hotmail.com>
2026-05-10 13:06:42 -0400

    feat(cli): migrate to esm and add cli entry point

--- core/example/tsdown.config.ts
+++ core/example/tsdown.config.ts
@@ -3,9 +3,15 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
 	attw: true,
 	dts: true,
-	entry: ["./src/index.ts"],
-	exports: true,
-	format: "cjs",
+	entry: {
+		cli: "./src/cli.ts",
+		index: "./src/index.ts",
+	},
+	exports: {
+		bin: true,
+		enabled: true,
+	},
+	format: "esm",
 	publint: true,
 	target: "esnext",
 });

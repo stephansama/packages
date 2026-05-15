15efa5b6bb156057f3e5af1dfa3097c4499c8123
Stephan Randle <stephanrandle@hotmail.com>
2026-05-14 23:38:17 -0400

    perf(core): update package manager and fix paths (#276)
    * perf(core): update package manager and fix paths
    
    * perf(core): fix order of enum values in schema snapshot
    
    * fix(core): update default table headings order in schema snapshot
    
    * feat(ci): add build:snapshot to turbo and package.json scripts
    
    * ci(prepare-local): exit early in ci environment

--- scripts/types/packagejson.d.ts
+++ scripts/types/packagejson.d.ts
@@ -2,6 +2,7 @@ import type {} from "@manypkg/tools";
 
 declare module "@manypkg/tools" {
 	export interface PackageJSON {
+		bin?: Record<string, string> | string;
 		description?: string;
 		files: Array<string>;
 		scripts?: Record<string, string>;

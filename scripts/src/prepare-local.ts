f060ebdad98c5e26db11152f5854f5d6c4c23a54
stephansama <stephanrandle@hotmail.com>
2026-05-14 22:47:16 -0400

    perf(core): update package manager and fix paths

--- scripts/src/prepare-local.ts
+++ scripts/src/prepare-local.ts
@@ -4,27 +4,24 @@ import { getPackages } from "@manypkg/get-packages";
 import * as cp from "node:child_process";
 import * as fs from "node:fs";
 import path from "node:path";
-
-if (process.env.CI) process.exit(0);
+import * as url from "node:url";
 
 const sh = String.raw;
 
 const { packages, rootPackage } = await getPackages(process.cwd());
 
-const allRootDependencies = {
-	...rootPackage?.packageJson.devDependencies,
-	...rootPackage?.packageJson.dependencies,
-};
-
 const stephansamaPackageNames = new Set(
-	Object.keys(allRootDependencies).filter((dependency) => {
-		return dependency.startsWith("@stephansama/");
-	}),
+	Object.keys({
+		...rootPackage?.packageJson.devDependencies,
+		...rootPackage?.packageJson.dependencies,
+	}).filter((dependency) => dependency.startsWith("@stephansama/")),
 );
 
 const stephansamaPackages = packages.filter((pkg) => {
 	return stephansamaPackageNames.has(pkg.packageJson.name);
 });
+
+const packageBins = new Array<string>();
 
 for (const pkg of stephansamaPackages) {
 	const hasAllOutputs = pkg.packageJson.files.every((file) => {
@@ -35,8 +32,22 @@ for (const pkg of stephansamaPackages) {
 
 	console.info(`running build for ${pkg.packageJson.name}`);
 
-	cp.execSync(sh`pnpm --filter='${pkg.packageJson.name}' run build`, {
-		encoding: "utf8",
-		stdio: "inherit",
-	});
+	if (pkg.packageJson.bin) {
+		const { bin } = pkg.packageJson;
+		const binName = pkg.packageJson.name.replace("@stephansama/", "");
+		const entries = typeof bin === "string" ? [binName] : Object.keys(bin);
+		packageBins.push(...entries);
+	}
+
+	const buildPackageCommand = sh`pnpm --filter='${pkg.packageJson.name}' run build`;
+
+	cp.execSync(buildPackageCommand, { encoding: "utf8", stdio: "inherit" });
+}
+
+const dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const nodeBinDirectory = path.resolve(dirname, "../../node_modules/.bin/");
+const nodeBins = await fs.promises.readdir(nodeBinDirectory);
+
+if (packageBins.some((bin) => !nodeBins.includes(bin))) {
+	cp.execSync(sh`pnpm install`, { stdio: "inherit" });
 }

802e2a1c4e3a08a5818c582e67831bd89fb73bfb
stephansama <stephanrandle@hotmail.com>
2026-05-17 23:31:54 -0400

    feat(core): add cleye for cli commands

--- core/auto-readme/src/cli.ts
+++ core/auto-readme/src/cli.ts
@@ -1,8 +1,36 @@
 #!/usr/bin/env node
 
+import { cli, command } from "cleye";
+
 import { generate } from "./generate";
 import { run } from "./index";
 
-const [subcommand, ...rest] = process.argv.slice(2);
+const generateCommand = command(
+	{
+		flags: {
+			force: {
+				alias: "f",
+				default: false,
+				description: "Overwrite the target file if it already exists",
+				type: Boolean,
+			},
+		},
+		name: "generate",
+		parameters: ["[filename]"],
+	},
+	async (argv) => {
+		const passthrough: string[] = [];
+		if (argv._.filename) passthrough.push(argv._.filename);
+		if (argv.flags.force) passthrough.push("--force");
+		await generate(passthrough);
+	},
+);
 
-await (subcommand === "generate" ? generate(rest) : run());
+const argv = cli({
+	commands: [generateCommand],
+	name: "auto-readme",
+});
+
+if (!argv.command) {
+	await run();
+}

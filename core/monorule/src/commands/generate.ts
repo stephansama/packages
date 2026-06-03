3ab573e584badd5dc0890c0fb6d9394cb79ee8dc
stephansama <stephanrandle@hotmail.com>
2026-06-02 21:17:30 -0400

    perf(core): add logging and placeholder for generate command

--- core/monorule/src/commands/generate.ts
+++ core/monorule/src/commands/generate.ts
@@ -1,12 +1,14 @@
 import { command } from "cleye";
 
-import type { cliArguments } from "@/src/cli";
+import type { CliArguments } from "@/src/cli";
 
 export const meta = command({
 	name: "generate",
 });
 
-export async function act(arguments_: typeof cliArguments) {
+export async function act(arguments_: CliArguments) {
+	await Promise.resolve({});
+	console.info(arguments_);
 	// TODO: generate default configuration
 	// TODO: generate rules directory
 }

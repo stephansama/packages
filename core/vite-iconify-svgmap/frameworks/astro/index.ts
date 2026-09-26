0481f752197fb038e703bea2eea2298107c0dcdb
stephansama <stephanrandle@hotmail.com>
2026-09-26 09:22:29 -0500

    feat(astro): add icon export

--- core/vite-iconify-svgmap/frameworks/astro/index.ts
+++ core/vite-iconify-svgmap/frameworks/astro/index.ts
@@ -1,2 +1,3 @@
 export { default as integration } from "../../dist/astro/integration.mjs";
+
 export { Icon } from "./component";
--- core/vite-iconify-svgmap/frameworks/svelte/index.d.ts
+++ core/vite-iconify-svgmap/frameworks/svelte/index.d.ts
@@ -1,2 +1,3 @@
 export { default as integration } from "../../dist/svelte/integration.mjs";
+
 export { Icon, type IconProps } from "./component";
